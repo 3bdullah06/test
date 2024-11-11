acd356b9d86342a5d57dce69b255b177bc136c98
Abdullah Abd El Ghany <3bdullah.eg@gmail.com>
2024-11-11 03:37:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -21,14 +21,6 @@ btn.addEventListener("click", function () {
 });
 
 
-let el = document.querySelector(".scroller");
-let height =
-  document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-window.addEventListener("scroll", () => {
-  let scrollTop = document.documentElement.scrollTop;
-  el.style.width = `${(scrollTop / height) * 100}%`;
-});
 
 let slideIndex = 1;
       showSlides(slideIndex);
